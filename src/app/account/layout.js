e831d0a63926d8c8de5c2c95ecf6fd468e31ef8c
Megan Palmquist <megpalmq@iu.edu>
2024-12-04 13:20:24 -0500

    adjusted font

--- src/app/account/layout.js
+++ src/app/account/layout.js
@@ -1,6 +1,5 @@
 // "use strict";
 
-<<<<<<< HEAD
 // Object.defineProperty(exports, "__esModule", {
 //   value: true,
 // });
@@ -35,39 +34,3 @@
 //     )
 //   );
 // }
-=======
-Object.defineProperty(exports, "__esModule", {
-  value: true,
-});
-exports.default = RootLayout;
-var _nextjs = require("@clerk/nextjs");
-//require("./globals.css");
-function RootLayout(_ref) {
-  var children = _ref.children;
-  return /*#__PURE__*/ React.createElement(
-    _nextjs.ClerkProvider,
-    null,
-    /*#__PURE__*/ React.createElement(
-      "html",
-      {
-        lang: "en",
-      },
-      /*#__PURE__*/ React.createElement(
-        "body",
-        null,
-        /*#__PURE__*/ React.createElement(
-          _nextjs.SignedOut,
-          null,
-          /*#__PURE__*/ React.createElement(_nextjs.SignInButton, null)
-        ),
-        /*#__PURE__*/ React.createElement(
-          _nextjs.SignedIn,
-          null,
-          /*#__PURE__*/ React.createElement(_nextjs.UserButton, null)
-        ),
-        children
-      )
-    )
-  );
-}
->>>>>>> 816afafdd3ba854fc645f448369c79ab68122e10
