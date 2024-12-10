1e9aa5c523574b1d6c2336bec5e3376f441c9d08
bakejona <jbakerbba2@gmail.com>
2024-12-10 11:16:08 -0500

    Added hero image and text to home page

--- src/app/page.js
+++ src/app/page.js
@@ -1,9 +1,17 @@
+// src/app/page.js
 import styles from "./page.module.css";
+
 export default function Home() {
   return (
     <>
       <main className={styles.main}>
-        <p>This is the main home page</p>
+        <section className={styles.hero}>
+          <div className={styles.heroContent}>
+            <h2>Welcome to</h2>
+            <h1>Curling You Say?</h1>
+            <p>Your ultimate guide to the sport of curling.</p>
+          </div>
+        </section>
       </main>
       <footer></footer>
     </>
